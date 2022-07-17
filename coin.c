74674d8f0ece3f3ad5f83fc95e5e1f0eea29999e
kavya kumar <53336752+kavya852@users.noreply.github.com>
2022-07-17 23:01:43 +0530

    Create coin.c

---
+++ coin.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+void main(void){
+    int n;
+    printf("Enter the no. of denominaitons: ");
+    scanf("%d", &n);
+    int d[n];
+    printf("Enter the denominations::\n");
+    for(int i=0; i<n; i++){
+            scanf("%d", &d[i]);
+    }
+    //sorting the denominations
+    for(int i=0; i<n-1; i++){
+        for(int j=0; j<n-i-1; j++){
+            if(d[j]<d[j+1]){
+                int t=d[j];
+                d[j]=d[j+1];
+                d[j+1]=t;
+            }
+        }
+    }
+    int v;
+    printf("\nEnter the value you want to achieve: ");
+    scanf("%d", &v);
+    int used[n]; int j=0; int total=0, cc=0;
+    for(int i=0; i<n; i++){
+            if(d[i]<=v && v>0){
+                used[j]=d[i];
+                j++;
+                v-=d[i];
+                total+=d[i];
+                cc++;
+            }
+    }
+    printf("\nTotal value achieved: %d\n", total);
+    printf("\nNo. of coins used: %d\n", cc);
+    printf("\nUsed coins::\n");
+    for(int i=0; i<j; i++){
+        printf("%d ", used[i]);
+    }
+    printf("\n");
+}
